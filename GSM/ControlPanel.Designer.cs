3674cb8d212e4c1e9d19b4e03e2a1ec240b15224
CJ8664 <johndoe@example.com>
2017-09-15 23:01:16 -0400

    Initial Commit

---
+++ GSM/ControlPanel.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GSM
+{
+    partial class ControlPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddUser = new System.Windows.Forms.Button();
+            this.DeleteUser = new System.Windows.Forms.Button();
+            this.AddCell = new System.Windows.Forms.Button();
+            this.DeleteCell = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddUser
+            // 
+            this.AddUser.Location = new System.Drawing.Point(28, 12);
+            this.AddUser.Name = "AddUser";
+            this.AddUser.Size = new System.Drawing.Size(228, 35);
+            this.AddUser.TabIndex = 0;
+            this.AddUser.Text = "Add User";
+            this.AddUser.UseVisualStyleBackColor = true;
+            // 
+            // DeleteUser
+            // 
+            this.DeleteUser.Location = new System.Drawing.Point(28, 62);
+            this.DeleteUser.Name = "DeleteUser";
+            this.DeleteUser.Size = new System.Drawing.Size(228, 42);
+            this.DeleteUser.TabIndex = 1;
+            this.DeleteUser.Text = "Delete User";
+            this.DeleteUser.UseVisualStyleBackColor = true;
+            // 
+            // AddCell
+            // 
+            this.AddCell.Location = new System.Drawing.Point(28, 120);
+            this.AddCell.Name = "AddCell";
+            this.AddCell.Size = new System.Drawing.Size(228, 41);
+            this.AddCell.TabIndex = 2;
+            this.AddCell.Text = "Add Cell";
+            this.AddCell.UseVisualStyleBackColor = true;
+            // 
+            // DeleteCell
+            // 
+            this.DeleteCell.Location = new System.Drawing.Point(28, 177);
+            this.DeleteCell.Name = "DeleteCell";
+            this.DeleteCell.Size = new System.Drawing.Size(228, 42);
+            this.DeleteCell.TabIndex = 3;
+            this.DeleteCell.Text = "Delete Cell";
+            this.DeleteCell.UseVisualStyleBackColor = true;
+            // 
+            // ControlPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.DeleteCell);
+            this.Controls.Add(this.AddCell);
+            this.Controls.Add(this.DeleteUser);
+            this.Controls.Add(this.AddUser);
+            this.Name = "ControlPanel";
+            this.Text = "ControlPanel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddUser;
+        private System.Windows.Forms.Button DeleteUser;
+        private System.Windows.Forms.Button AddCell;
+        private System.Windows.Forms.Button DeleteCell;
+    }
+}
---
+++ GSM/Main.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace GSM
+{
+    public partial class Main : Form
+    {
+        public LinkedList<Cell> cells;
+        public static Graphics bitmapGraphics;
+        public Pen p;
+        public ControlPanel control;
+        public static int maxWidth, maxHeight;
+
+        public Main()
+        {
+            InitializeComponent();
+            maxWidth = 600;
+            maxHeight = 600;
+            control = new ControlPanel();
+            control.Show();
+        }
+
+        private void Main_Paint(object sender, PaintEventArgs e)
+        {
+            cells = new LinkedList<Cell>();
+            Main.bitmapGraphics = e.Graphics;
+            p = new Pen(Color.Black);
+
+            float x = 20, y = 0, side = 40;
+            int sign = 1;
+
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 1; j < 5; j++)
+                {
+                    Cell c = new Cell(x + (j-1)*3*side, y);
+                    c.drawCell(bitmapGraphics, p);
+                    cells.AddLast(c);
+                }
+                y = y + (float)(Math.Cos(30 * Math.PI / 180) * side);
+                x = x + (float)(sign * 1.5 * side);
+                sign *= -1;
+            }
+
+            addUser();
+            /*
+            Bitmap bitmap = new Bitmap(400 , 400);
+            Graphics bitmapGraphics = Graphics.FromImage(bitmap);
+            SolidBrush sb = new SolidBrush(Color.Black);
+            Graphics graphicsObject = e.Graphics;
+            Rectangle drawArea1 = new Rectangle( 5, 35, 30, 100 );
+            LinearGradientBrush linearBrush = new LinearGradientBrush(drawArea1, Color.Blue, Color.Yellow, LinearGradientMode.ForwardDiagonal);
+            //draw ellipse filled with a blue-yellow gradient
+         	graphicsObject.FillEllipse( linearBrush, 5, 30, 65, 100 );
+             */
+        }
+
+        public void addCell(float x, float y)
+        {
+            p = new Pen(Color.Black);
+            Cell c = new Cell(x , y);
+            c.drawCell(Main.bitmapGraphics, p);
+            cells.AddLast(c);
+        }
+
+        public void deleteCell(float x, float y)
+        {
+            p = new Pen(Color.White);
+            Cell c = new Cell(x, y);
+            c.drawCell(Main.bitmapGraphics, p);
+            //cells.AddLast(c);
+        }
+
+        public void addUser()
+        {
+            int x = 1;
+            int y = 10;
+            
+            while (true)
+            {
+                p = new Pen(Color.Black);
+                Point p1 = new Point(x+10,y+10);
+                Point p2 = new Point(x+10,y+11);
+                //bitmapGraphics.DrawString(".", new Font("Microsoft Sans Serif",9), 
+                bitmapGraphics.DrawLine(p, p1, p2);
+
+                x++;
+            }
+        }
+
+        public void deleteUser()
+        {
+        }
+    }
+
+    public class Cell
+    {
+        float x;  //X co-ordinate of left top of cell
+        float y;  //Y co-ordinate of left top of cell
+        float side = 40;    //Length of side of hexagon	
+        float h = (float)(Math.Sin(30 * Math.PI / 180) * 40); //20
+        float r = (float)(Math.Cos(30 * Math.PI / 180) * 40); //35
+        PointF[] points;
+
+        public Cell(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+            points = new PointF[6];
+        }
+
+        public void drawCell(Graphics s, Pen p)
+        {
+            //Console.Write(Main.ActiveForm.Width + " " + Main.ActiveForm.Height + "Printed" + this.x + this.y); 
+            points[0] = new PointF(x, y);
+            points[1] = new PointF(x + side, y);
+            points[2] = new PointF(x + side + h, y + r);
+            points[3] = new PointF(x + side, y + r + r);
+            points[4] = new PointF(x, y + r + r);
+            points[5] = new PointF(x - h, y + r);
+
+            if (this.x >= 0 && this.x <= (Main.maxWidth - 2*side) && this.y >= 0 && this.y <= (Main.maxHeight - 2*side))
+            {
+                s.DrawPolygon(p, points);
+            }
+            else
+            {
+                MessageBox.Show("Cannot create a cell at specified location");
+            }
+            //Console.WriteLine(h + " " + r + " " + points[0] + " " + points[1] + " " + points[2] + " " + points[3] + " " + points[4] + " " + points[5] + " ");
+        }
+    }
+}
